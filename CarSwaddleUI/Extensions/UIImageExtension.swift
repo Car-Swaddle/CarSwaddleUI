d0d5205807cb770ecc53afec570e42b68e0e6f9e
Kyle Kendall <kkendall33@gmail.com>
2019-01-07 23:59:38 -0700

    add image stuff

--- CarSwaddleUI/Extensions/UIImageExtension.swift
+++ CarSwaddleUI/Extensions/UIImageExtension.swift
@@ -37,5 +37,21 @@ public extension UIImage {
         return normalizedImage
     }
     
+    public func resized(withPercentage percentage: CGFloat) -> UIImage? {
+        let canvasSize = CGSize(width: size.width * percentage, height: size.height * percentage)
+        UIGraphicsBeginImageContextWithOptions(canvasSize, false, scale)
+        defer { UIGraphicsEndImageContext() }
+        draw(in: CGRect(origin: .zero, size: canvasSize))
+        return UIGraphicsGetImageFromCurrentImageContext()
+    }
+    
+    public func resized(toWidth width: CGFloat) -> UIImage? {
+        let canvasSize = CGSize(width: width, height: CGFloat(ceil(width/size.width * size.height)))
+        UIGraphicsBeginImageContextWithOptions(canvasSize, false, scale)
+        defer { UIGraphicsEndImageContext() }
+        draw(in: CGRect(origin: .zero, size: canvasSize))
+        return UIGraphicsGetImageFromCurrentImageContext()
+    }
+    
 }
 
