1c324a16daaf53f3475d58a32faef9ee1a5e2929
Kyle Kendall <kkendall33@gmail.com>
2019-02-01 22:56:12 -0700

    Tweaks

---
+++ CarSwaddleUI/Tweaks/TweakViewController.swift
@@ -0,0 +1,85 @@
+//
+//  TweakViewController.swift
+//  CarSwaddleMechanic
+//
+//  Created by Kyle Kendall on 1/27/19.
+//  Copyright © 2019 CarSwaddle. All rights reserved.
+//
+
+import UIKit
+import CarSwaddleUI
+
+protocol TweakViewControllerDelegate: AnyObject {
+    func didDismiss(requiresAppReset: Bool, tweakViewController: TweakViewController)
+}
+
+final class TweakViewController: UIViewController, StoryboardInstantiating {
+    
+    class func create(with tweaks: [Tweak], delegate: TweakViewControllerDelegate? = nil) -> TweakViewController {
+        let viewController = TweakViewController.viewControllerFromStoryboard()
+        viewController.tweaks = tweaks
+        viewController.delegate = delegate
+        return viewController
+    }
+    
+    public weak var delegate: TweakViewControllerDelegate?
+    
+    @IBOutlet private weak var tableView: UITableView!
+    
+    private var tweaks: [Tweak] = []
+    private var resetAppOnDismiss: Bool = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        tableView.register(TweakStringValueCell.self)
+        tableView.tableFooterView = UIView()
+    }
+    
+    @IBAction func didSelectDone(_ button: UIBarButtonItem) {
+        let shouldReset = resetAppOnDismiss
+        dismiss(animated: true) { [weak self] in
+            guard let self = self else { return }
+            self.delegate?.didDismiss(requiresAppReset: shouldReset, tweakViewController: self)
+        }
+    }
+}
+
+extension TweakViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tweaks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: TweakStringValueCell = tableView.dequeueCell()
+        
+        let tweak = tweaks[indexPath.row]
+        cell.configure(with: tweak)
+        return cell
+    }
+    
+}
+
+extension TweakViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let viewController = TweakSelectViewController.create(with: tweaks[indexPath.row])
+        viewController.delegate = self
+        show(viewController, sender: true)
+    }
+    
+}
+
+extension TweakViewController: TweakSelectDelegate {
+    
+    func didChangeValue(tweak: Tweak, newValue: Any?, tweakSelectViewController: TweakSelectViewController) {
+        resetAppOnDismiss = tweak.requiresAppReset
+        tableView.reloadData()
+    }
+    
+}
