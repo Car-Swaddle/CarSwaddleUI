c9e8dd59c2c91344ad7757d6c6f114e3a8ef974f
Kyle Kendall <kkendall33@gmail.com>
2020-09-19 01:31:39 -0600

    Yo

--- Package.swift
+++ Package.swift
@@ -19,6 +19,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        // Test this comment
         .target(
             name: "CarSwaddleUI",
             dependencies: ["Lottie"]),
